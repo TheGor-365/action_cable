7c1481c2202d94314a1509f7d0c41cf3a78ee796
Gor <61106836+TheGor-365@users.noreply.github.com>
2023-02-25 02:34:46 +0300

    Add drawning javascript logic

---
+++ app/channels/draw_channel.rb
@@ -0,0 +1,13 @@
+class DrawChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "draw_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def draw
+    ActionCable.server.broadcast "draw_channel", data
+  end
+end
